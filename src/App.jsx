b916709ef6daa8e4020d79a8970dcd82a1728938
Loreto <loreto.garde@gmail.com>
2026-02-28 01:12:14 +0100

    refactor(app): remove unused Navigate import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import RequireAuth from "./auth/requireAuth";
 
 import { Navbar } from "./components/Navbar";
